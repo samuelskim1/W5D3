d883091e7850ccb365fa80fffc604c1bbb402250
Samuel Kim <samuelskim420@gmail.com>
2023-02-08 22:11:09 +0000

    finished replies

--- import_db.sql
+++ import_db.sql
@@ -1,4 +1,4 @@
-PRAGMA foreign_keys = ON
+PRAGMA foreign_keys = ON;
 
 DROP TABLE IF EXISTS question_likes;
 DROP TABLE IF EXISTS replies;
@@ -12,7 +12,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
 
@@ -30,11 +30,11 @@ CREATE TABLE questions (
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
-    users_id TEXT NOT NULL,
-    questions_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    question_id TEXT NOT NULL,
 
-    FOREIGN KEY (users_id) REFERENCES users(id)
-    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 
 );
 
@@ -43,11 +43,11 @@ CREATE TABLE question_follows (
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
-    parent_reply_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
     body TEXT NOT NULL,
 
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
-    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 
 );
 
@@ -65,36 +65,39 @@ CREATE TABLE question_likes (
 );
 
 INSERT INTO
-    users(fname, lname)
+    users (fname, lname)
 VALUES
     ('Bruce', 'Willis'),
     ('Anya', 'Taylor-Joy'),
-    ('James', 'Bond')
-    ('Elizabeth', 'Taylor')
+    ('James', 'Bond'),
+    ('Elizabeth', 'Taylor'),
     ('Brad', 'Pitt');
 
 INSERT INTO
-    questions(author_id, title, body)
+    questions (author_id, title, body)
 VALUES
     ((SELECT id FROM users WHERE users.fname = 'James' AND users.lname = 'Bond'), 'Oscars?', 'Who won the Best Movie last year?'),
     ((SELECT id FROM users WHERE users.fname = 'Brad' AND users.lname = 'Pitt'), 'Snacks?', 'Anyone have a snack for me?'),
     ((SELECT id FROM users WHERE users.fname = 'Anya' AND users.lname = 'Taylor-Joy'), 'Ballet?', 'Who can teach me ballet?');
 
 INSERT INTO
-    question_follows(users_id, question_id)
+    question_follows (user_id, question_id)
 VALUES
-    (SELECT users.id FROM users WHERE users.fname = 'James' AND users.lname = 'Bond', 
-        SELECT questions.id FROM questions WHERE questions.title = "Snacks?"),
-    (SELECT users.id FROM users WHERE users.fname = 'Anya' AND users.lname = 'Taylor-Joy', 
-        SELECT questions.id FROM questions WHERE questions.title = "Snacks?"),
-    (SELECT users.id FROM users WHERE users.fname = 'Brad' AND users.lname = 'Pitt', 
-        SELECT questions.id FROM questions WHERE questions.title = "Ballet?");
+    ((SELECT users.id FROM users WHERE users.fname = 'James' AND users.lname = 'Bond'), 
+        (SELECT questions.id FROM questions WHERE questions.title = "Snacks?")),
+    ((SELECT users.id FROM users WHERE users.fname = 'Anya' AND users.lname = 'Taylor-Joy'), 
+        (SELECT questions.id FROM questions WHERE questions.title = "Snacks?")),
+    ((SELECT users.id FROM users WHERE users.fname = 'Brad' AND users.lname = 'Pitt'), 
+        (SELECT questions.id FROM questions WHERE questions.title = "Ballet?"));
     
 
 
--- INSERT INTO
---     replies(question_id, parent_reply_id, body)
--- VALUES
---     ()
+INSERT INTO
+    replies(question_id, body)
+VALUES
+    ((SELECT questions.id FROM questions WHERE questions.title = "Snacks?"),
+        'snickers and twix!'),
+    ((SELECT questions.id FROM questions WHERE questions.title = "Ballet?"),
+    'Tonight!')
 
 
